423e1a0ed502f705588e81ba8d036fbaf830447a
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2021-11-11 15:21:16 +0800

    calculate climate impacts

--- AquaMaps/Calculation/cc_calc.R
+++ AquaMaps/Calculation/cc_calc.R
@@ -1,7 +1,11 @@
+# calculate climate impacts
+
 library(raster)
 library(magrittr)
 
-setwd('D:/Users/Yuxuan Lin/Documents/LocalFiles/XMU/Connectivity/AquaMaps')
+setwd('')
+from_dir <- ''
+to_dir <- ''
 
 func <- function(x,lim_lower,lim_upper){
   x$cc <- 0
@@ -16,22 +20,16 @@ func <- function(x,lim_lower,lim_upper){
 # load files
 lower_sd <- read.csv('tolerance/lower_sd.csv')
 upper_sd <- read.csv('tolerance/upper_sd.csv')
-spelist <- read.csv('distribution/historical/Species_list.csv')
+spelist <- read.csv('Species_list.csv')
 
 # create parameter list
 paras <- colnames(lower_sd)[-c(1,2)]
 
-# create year list
-# years <- seq.int(2015,2050)
-# years <- append(years,2100)
-years <- seq.int(2015,2020)
-years <- c(2021)
-
-# set scenario
-# c('ssp126','ssp245','ssp370','ssp585')
+# initialization
 scen <- 'ssp585'
+years <- 2020
 
-for (r in 21955:nrow(spelist)){
+for (r in 1:nrow(spelist)){
   # extract information
   exist <- spelist[r,] %>% as.character()
   spe <- exist[1]
@@ -41,43 +39,39 @@ for (r in 21955:nrow(spelist)){
   distri <- colnames(spelist)[which(exist != 'NA')][-c(1,2)]
   for (d in distri){
     # input species raster
-    spe_rs <- raster(paste0('SpeciesDistribution/',d,'/',phy,'/',spe,'.tif'))
+    spe_rs <- raster(paste0(from_dir,'/',d,'/',phy,'/',spe,'.tif'))
     
-    for (yr in years){
-      # create environment list
-      envlist <- c()
+    # create environment list
+    envlist <- c()
+    for (p in paras){
+      envname <- paste0(p,'_',d,'_',scen,'_',yr,'.tif')
+      envlist <- append(envlist, paste0('cmip6_future/',d,'/',scen,'/',p,'/',envname))
+    }
+    # create realized niches each year
+    env_rs <- stack(envlist)
+    dt <- mask(env_rs, spe_rs) %>% as.data.frame(., xy=T) %>% .[complete.cases(.),]
+    colnames(dt)[3:19] <- paras
+    
+    if (nrow(dt) > 0){
+      row.names(dt) <- seq.int(1:nrow(dt))
+      
+      # calculate climate exposure
+      lonlat <- dt[,1:2]
       for (p in paras){
-        envname <- paste0(p,'_',d,'_',scen,'_',yr,'.tif')
-        envlist <- append(envlist, paste0('cmip6_future/',d,'/',scen,'/',p,'/',envname))
+        # extract species tolerance
+        upperlim <- upper_sd[r,p]
+        lowerlim <- lower_sd[r,p]
+        # calculation
+        x <- dt[,c('x','y',p)]
+        cal <- func(x,lowerlim,upperlim)
+        lonlat <- cbind(lonlat,cal[,4])
       }
-      # create realized niches each year
-      env_rs <- stack(envlist)
-      dt <- mask(env_rs, spe_rs) %>% as.data.frame(., xy=T) %>% .[complete.cases(.),]
-      colnames(dt)[3:19] <- paras
+      colnames(lonlat)[3:19] <- paras
       
-      if (nrow(dt) > 0){
-        row.names(dt) <- seq.int(1:nrow(dt))
-        
-        # calculate climate exposure
-        lonlat <- dt[,1:2]
-        for (p in paras){
-          # extract species tolerance
-          upperlim <- upper_sd[r,p]
-          lowerlim <- lower_sd[r,p]
-          # calculation
-          x <- dt[,c('x','y',p)]
-          cal <- func(x,lowerlim,upperlim)
-          lonlat <- cbind(lonlat,cal[,4])
-        }
-        colnames(lonlat)[3:19] <- paras
-        
-        # export 
-        write.csv(lonlat, paste0('ClimateImpact/',scen,'/',yr,'/',d,'/',phy,'/',spe,'.csv'), row.names = F)
-        print(paste0(yr,'-',d,'-',phy,'-',spe,'- complete'))
-      }
-      
+      # export 
+      write.csv(lonlat, paste0(to_dir,'/',scen,'/',yr,'/',d,'/',phy,'/',spe,'.csv'), row.names = F)
+      print(paste0(yr,'-',d,'-',phy,'-',spe,'- complete'))
     }
   }
 }
-
 
