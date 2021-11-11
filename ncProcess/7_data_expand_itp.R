0a85a31e920114886bf2a4b0aee0c906b5c2378d
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2021-11-11 10:25:53 +0800

    Update 7_data_expand_itp.R

--- ncProcess/7_data_expand_itp.R
+++ ncProcess/7_data_expand_itp.R
@@ -1,10 +1,12 @@
 library(magrittr)
+library(sp)
 library(raster)
 library(sf)
 
 
 # ==============================
-# average all among depth layers
+# expand extent by several degrees
+# preprocessing for interpolation 
 
 
 rs_exp <- function(rs, output_path){
@@ -28,27 +30,44 @@ rs_exp <- function(rs, output_path){
   writeRaster(rs, output_path, overwrite=T) 
 }
 
-
-# c('surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic')
-for (h in c('mesopelagic')){
-  from_dir <- paste0("D:/Users/Yuxuan Lin/Documents/LocalFiles/6_data_smooth/", h)
-  to_dir <- paste0("D:/Users/Yuxuan Lin/Documents/LocalFiles/7_data_expand_itp/", h)
-  
-  for (s in c('historical', 'ssp126', 'ssp245', 'ssp370', 'ssp585')){
-    for (p in list.files(paste0(from_dir,'/', s))){
-      files <- list.files(paste0(from_dir,'/',s,'/',p), pattern = '*.tif$')
-      for (f in files){
-        output_path <- paste0(to_dir,'/',s,'/',p,'/',f)
-        input_path <- paste0(from_dir,'/',s,'/',p,'/',f)
-        raster(input_path) %>%
-          rs_exp(., output_path)
-        
-      }
-      print(paste0(s, ': ', p, ' complete'))
+setwd('')
+for (h in c('surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic')){
+  from_dir <- paste0("6_data_smooth/", h)
+  to_dir <- paste0("7_data_expand_itp/", h)
+  for (s in c('ssp126', 'ssp245', 'ssp370', 'ssp585')){
+    files <- list.files(paste0(from_dir,'/',s), pattern = '*.tif$')
+    for (f in files){
+      output_path <- paste0(to_dir,'/',s,'/',f)
+      input_path <- paste0(from_dir,'/',s,'/',f)
+      raster(input_path) %>%
+        rs_exp(., output_path)
     }
-    
+    print(paste0(s, ': ', f, ' complete'))
   }
   print(paste0(h, ': complete ----'))
 }
 
+
+
+# ==============================
+# convert raster to point
+# preprocessing for interpolation 
+
+
+setwd('')
+for (depth in c('surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic')){
+  for (scen in list.files(paste0('7_data_expand_itp/',depth))){
+    for (f in list.files(paste0('7_data_expand_itp/',depth,'/',scen))){
+      fromdir <- paste0('7_data_expand_itp/',depth,'/',scen,'/',f)
+      rs <- raster(fromdir) %>% as.data.frame(., xy=T) %>% .[complete.cases(.),]
+      colnames(rs)[3] <- 'value'
+      fname <- strsplit(f,'.tif')[[1]]
+      
+      # convert to point
+      rs.sf <- st_as_sf(rs, coords = c('x','y'), crs=4326)
+      st_write(rs.sf, paste0('8_rs2point/',depth,'/',scen,'/',fname,'.shp'))
+    }
+    print(paste0(depth,'-',scen,': complete'))
+  }
+}
 
