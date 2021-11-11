a01e4888d73581d346ec6c0e3cbcd41fc4b5d6da
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2021-11-11 14:17:33 +0800

    convert csv to TIFF

--- AquaMaps/step2_rasterize.R
+++ AquaMaps/step2_rasterize.R
@@ -15,10 +15,6 @@ rasterize_func <- function(dt, fieldv){
   return (rs)
 }
 
-# built a null raster
-# null <- raster(crs = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0", 
-#                vals = 0, resolution = c(0.5, 0.5), ext = extent(c(-180, 180, -90, 90)))
-
 
 setwd('')
 to_dir <- ''
