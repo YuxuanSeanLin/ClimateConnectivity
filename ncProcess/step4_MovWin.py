af7a9ac04e1b456cfa5d1d406961511e9cad4c13
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2021-11-11 10:13:12 +0800

    Update and rename 6_data_smooth.py to step4_MovWin.py

---
+++ ncProcess/step4_MovWin.py
@@ -0,0 +1,21 @@
+## arcpy focal statisics
+
+import os
+from glob import glob
+import arcpy
+from arcpy import env
+
+wd = ''
+for depth in ['surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic']:
+    for scen in ['ssp126', 'ssp245', 'ssp370', 'ssp585']:
+        from_path = '%s/5_data_expand/%s/%s' % (wd, depth, scen)
+        to_path = '%s/6_data_smooth/%s/%s' % (wd, depth, scen)
+        for f in os.listdir(from_path):
+            masktif = '%s/4_data_mdmean/%s/%s/%s' % (wd, depth, scen, f)
+            with arcpy.EnvManager(mask=masktif):
+                out_raster = arcpy.ia.FocalStatistics("%s/%s" % (from_path, f), 
+                                                      "Rectangle 3 3 CELL", "MEAN"); 
+                out_raster.save("%s/%s" % (to_path, f))
+            print("%s finish" % f)
+        print("======== %s - %s - %s : complete ========" % (depth, scen))
+
