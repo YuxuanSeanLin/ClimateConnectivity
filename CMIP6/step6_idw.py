7774beb42bf4f1a32bdca56e6ca128225050d257
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2021-11-12 09:58:39 +0800

    IDW interpolation

--- CMIP6/step6_idw.py
+++ CMIP6/step6_idw.py
@@ -6,15 +6,17 @@ from arcpy import env
 wd = ''
 for depth in ['surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic']:
     for scen in ['ssp126', 'ssp245', 'ssp370', 'ssp585']:
-        env.workspace = '%s/8_rs2point/%s/%s' % (wd, depth, scen)
-        os.chdir('%s/8_rs2point/%s/%s' % (wd, depth, scen))
+        env.workspace = '%s/thetao_exp2p/%s/%s' % (wd, depth, scen)
+        os.chdir('%s/thetao_exp2p/%s/%s' % (wd, depth, scen))
         files = sorted(glob("*shp"))
         for f in files:
             name = f.split('.shp')[0]
-            to_path = "%s/9_idw/%s/%s/%s.tif" % (wd, depth, scen, name)
+            to_path = "%s/thetao_idw/%s/%s/%s.tif" % (wd, depth, scen, name)
 
             with arcpy.EnvManager(mask="bathymetry/%s.tif" % depth, extent="-180 -90 180 90"):
                 arcpy.gp.Idw_sa(f, "value", to_path, "1", "3", "VARIABLE 12", "")
 
         print("%s: %s --- complete" % (depth, scen))
+        
 
+        
