e95b16ad1c0007370fdc8b310ca293164ed85a15
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2021-11-19 22:48:36 +0800

    calculate climate velocity

--- CMIP6/step7_vocc.R
+++ CMIP6/step7_vocc.R
@@ -20,10 +20,18 @@ for (h in c('surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic')){
     # import raster stack
     rs <- stack(list)
     
+    # extend data
+    rs1 <- crop(rs, extent(-180, -179, -90, 90))
+    rs2 <- crop(rs, extent(179, 180, -90, 90))
+    extent(rs1) <- c(180, 181, -90, 90)
+    extent(rs2) <- c(-181, -180, -90, 90)
+    newrs <- merge(rs2, rs, rs1)
+    
     # calculate climate velocity
-    vt <- tempTrend(rs, th = 20)
-    vg <- spatGrad(rs, th = 0.0001, projected = FALSE)
+    vt <- tempTrend(newrs, th = 20)
+    vg <- spatGrad(newrs, th = 0.0001, projected = FALSE)
     gv <- gVoCC(vt, vg)
+    gv <- crop(gv, extent(-180, 180, -90, 90))
     
     # output
     writeRaster(gv, paste0('vocc/',h,'/vocc_',h,'_',s,'.tif'))
