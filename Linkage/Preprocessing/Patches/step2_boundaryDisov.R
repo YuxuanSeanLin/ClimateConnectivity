9d2306e5d9c78daf552cd90889b425d977690a35
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-01-12 20:19:57 +0800

    dissolve spatially-connected patches

--- Linkage/Preprocessing/Patches/step2_boundaryDisov.R
+++ Linkage/Preprocessing/Patches/step2_boundaryDisov.R
@@ -3,37 +3,37 @@ library(raster)
 library(magrittr)
 library(dplyr)
 
-
 #######
+# dissolve the spatially-connected multi-part patches
 
-setwd('D:/Users/Yuxuan Lin/Documents/LocalFiles/XMU/Connectivity/Linkage/patchtif/test0110')
+setwd('')
 
-# patch
-files <- list.files('patches_id2rs', pattern = '.tif$', recursive = T, full.names = T)
+## patch raster (f)
+files <- list.files('step1_patches_id2rs', pattern = '.tif$', recursive = T, full.names = T)
 for (f in files){
-  # class
-  f1 = paste0('patches_raw/', strsplit(f,'patches_id2rs/')[[1]][2])
+  ## class raster (f1)
+  f1 = paste0('patches_raw/', strsplit(f,'step1_patches_id2rs/')[[1]][2])
   
-  # combine class and patch id
+  ## combine class and patch id
   rs <- stack(f1, f)
   dt_all <- as.data.frame(rs, xy=T)
   colnames(dt_all)[3] <- 'class'
   colnames(dt_all)[4] <- 'patch'
   
-  # extract boundaries
+  ## extract boundaries
   dt_left <- subset(dt_all, x == -179.5)
   row.names(dt_left) <- seq(1, nrow(dt_left))
   dt_right <- subset(dt_all, x == 179.5)
   row.names(dt_right) <- seq(1, nrow(dt_right))
   
-  # scan all rows
+  ## scan all rows
   id = -1
   for (r in 1:nrow(dt_left)){
     left_class <- dt_left$class[r]
     right_class <- dt_right$class[r]
-    # judge NA
+    ## judge NA
     if (is.na(left_class)==F & is.na(right_class)==F){
-      # judge class
+      ## judge whether adjacent patches (at both sides of boundaries) have the same class
       if (left_class==right_class){
         # cover initial patch id with new one
         dt_all[which(dt_all$patch==dt_left$patch[r]), 'patch'] <- id
@@ -43,7 +43,7 @@ for (f in files){
     }
   }
   
-  # rasterize
+  ## rasterize
   dt_all <- dt_all[complete.cases(dt_all),]
   rs.sf <- st_as_sf(dt_all, coords = c('x','y'), crs=4326)
   rs.sp <- as(rs.sf, "Spatial") 
@@ -51,10 +51,10 @@ for (f in files){
                ext = extent(c(-180, 180, -90, 90))) %>%
     rasterize(rs.sp, ., field='patch', fun='first')  
   
-  # convert to shp (dissolve the same patch id)
+  ## convert to shp (dissolve the same patch id)
   sf.poly <- rasterToPolygons(newrs, dissolve=T) %>% st_as_sf(.)
   
-  # calculate shape area
+  ## calculate shape area (in decimal degree)
   dt_area <- c()
   for (i in unique(dt_all$patch)){
     dt_area <- rbind(dt_area, 
@@ -62,13 +62,11 @@ for (f in files){
       as.data.frame()
   }
   
-  # join by patch id
+  ## join attribute table by patch id
   sf.poly <- left_join(sf.poly, dt_area, by='layer')
   
-  # export shp
-  to_path <- paste0(strsplit(paste0('patches_newid/',strsplit(f,'patches_id2rs/')[[1]][2]),'.tif')[[1]][1], '.shp')
-  st_write(sf.poly, to_path)
+  ## export shp to <step2_patches_newid>
+  st_write(sf.poly, paste0('step2_patches_newid/',<###>))
 }
-
 
 
