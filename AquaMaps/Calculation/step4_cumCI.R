96cf149e54627418612f1866447d164c99d54651
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-02-06 11:16:43 +0800

    Create step4_cumCI.R

---
+++ AquaMaps/Calculation/step4_cumCI.R
@@ -0,0 +1,42 @@
+library(raster)
+
+setwd('')
+timestart<-Sys.time()
+
+for (year in seq(2030,2050,10)){
+  
+  for (dp in c("surface","mesopelagic","abyssopelagic","bathypelagic")){
+    # import seascape boundary
+    mask_r <- raster(paste0("CI_calculate/mask/",dp,'.tif'))
+    
+    # import depth canvas (180*360 raster, value=0)
+    canvas <- raster(paste0("CI_calculate/canvas/",dp,'.tif'))
+    
+    for (phy in list.files(paste0("CI_calculate/weight/",year,"/",dp))){
+      # list of all species
+      files <- list.files(paste0("CI_calculate/weight/",year,"/",dp,"/",phy), 
+                          pattern = '.tif$', full.names = T)
+      
+      for (f in files){
+        f <- raster(f)
+        # expand extend by NA (only rasters with the same extent can be summed)
+        f <- extend(f,extent(-180, 180, -90, 90),value=NA)
+        # sum all
+        canvas <- sum(canvas,f, na.rm = T)
+      }
+      print(paste(exp,year,dp,phy,sep = "-"))
+    }
+    
+    # extract canvas by mask (boundary)
+    canvas_r <- mask(canvas, mask_r)
+    
+    # export
+    writeRaster(canvas_r, paste0("CI_calculate/cum_CI/",dp,"/",year,".tif"),overwrite = TRUE)
+    print(paste(exp,"-",year,"-",dp,"Done!",sep = ""))
+  }
+}
+
+timeend<-Sys.time()
+runningtime <- round(timeend-timestart,1)
+print(runningtime)
+
