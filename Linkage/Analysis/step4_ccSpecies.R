0feaf09e79a4599bba40f88d1e52c3ed1f7c155e
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-02-10 17:47:18 +0800

    calculate species-based connectivity and stress

--- Linkage/Analysis/step4_ccSpecies.R
+++ Linkage/Analysis/step4_ccSpecies.R
@@ -33,58 +33,61 @@ for (h in c('surface','mesopelagic','bathypelagic','abyssopelagic')){
     colnames(spe_bypatch)[3] <- 'pid'
     
     ## calculate patch-wise species climate connectivity of 4 scenarios
-    for (s in c('ssp126','ssp245','ssp370','ssp585')){
-      spe_cc <- c() %>% as.data.frame()
-      
-      ### start with different destinations
-      for (pch in unique(spe_bypatch$pid)){
-        ### extract species within each patch
-        spe_bp <- subset(spe_bypatch, pid==pch)
+    if (nrow(spe_bypatch)>0){
+      for (s in c('ssp126','ssp245','ssp370','ssp585')){
+        spe_cc <- c() %>% as.data.frame()
         
-        ### load linkage data
-        links <- read.csv(paste0('ClimCon/',h,'/',s,'/cclink_2020/cclinks_',pch,'.csv'))
-        
-        ### extract designated patches that within tolerance edges
-        Dest_temp <- links[,(ncol(links)/2+2):ncol(links)] %>% 
-          as.matrix() %>% as.numeric() %>% na.omit() %>% unique()
-        
-        ### calculate climate connectivity and climate impacts (against climate of designated patches)
-        if (length(which(Dest_temp>=low_end & Dest_temp<=high_end))>0){ #### has suitable destinations
-          Dest_temp <- Dest_temp[which(Dest_temp>=low_end & Dest_temp<=high_end)] #### highest extremes (capability)
-          spe_bp$ClimCon <- unique(links$From_pre) - min(Dest_temp)
-          spe_bp$ClimImp <- 0
-        }else{ #### has no suitable destinations
-          #### targeted patches with lowest extremes (minimized stress)
-          dest_id <- which(abs(unique(links$From_pre)-Dest_temp)==min(abs(unique(links$From_pre)-Dest_temp)))
-          #### calculation
-          spe_bp$ClimCon <- abs(unique(links$From_pre)-Dest_temp[dest_id])*(-1)
-          if (Dest_temp[dest_id]>high_end){
-            spe_bp$ClimImp <- (Dest_temp[dest_id]-high_end)/(high_end-low_end)
-          }else{
-            spe_bp$ClimImp <- (low_end-Dest_temp[dest_id])/(high_end-low_end)
+        ### start with different destinations
+        for (pch in unique(spe_bypatch$pid)){
+          ### extract species within each patch
+          spe_bp <- subset(spe_bypatch, pid==pch)
+          
+          ### load linkage data
+          links <- read.csv(paste0('ClimCon/',h,'/',s,'/cclink_2020/cclink_',pch,'.csv'))
+          
+          ### extract designated patches that within tolerance edges
+          Dest_temp <- links[,(ncol(links)/2+2):ncol(links)] %>% 
+            as.matrix() %>% as.numeric() %>% na.omit() %>% unique()
+          
+          ### calculate climate connectivity and climate impacts (against climate of designated patches)
+          if (length(which(Dest_temp>=low_end & Dest_temp<=high_end))>0){ #### has suitable destinations
+            Dest_temp <- Dest_temp[which(Dest_temp>=low_end & Dest_temp<=high_end)] #### highest extremes (capability)
+            spe_bp$ClimCon <- unique(links$From_pre) - min(Dest_temp)
+            spe_bp$ClimImp <- 0
+          }else{ #### has no suitable destinations
+            #### targeted patches with lowest extremes (minimized stress)
+            dest_id <- which(abs(unique(links$From_pre)-Dest_temp)==min(abs(unique(links$From_pre)-Dest_temp)))
+            #### calculation
+            spe_bp$ClimCon <- abs(unique(links$From_pre)-Dest_temp[dest_id])*(-1)
+            if (Dest_temp[dest_id]>high_end){
+              spe_bp$ClimImp <- (Dest_temp[dest_id]-high_end)/(high_end-low_end)
+            }else{
+              spe_bp$ClimImp <- (low_end-Dest_temp[dest_id])/(high_end-low_end)
+            }
           }
+          ### combine table
+          spe_cc <- rbind(spe_cc, spe_bp)
         }
-        ### combine table
-        spe_cc <- rbind(spe_cc, spe_bp)
+        
+        ### export as raster files
+        rs.sp <- st_as_sf(spe_cc, coords = c('x','y'), crs=4326) %>% 
+          as(., "Spatial") 
+        #### climate connectivity
+        raster(crs = crs(rs.sp), vals = 0, resolution = c(1, 1), ext = extent(c(-180, 180, -90, 90))) %>%
+          rasterize(rs.sp, ., field='ClimCon', fun='first') %>% 
+          writeRaster(., paste0('ClimCon_species/Species_ClimCon/',s,
+                                strsplit(spe,'SpeciesDistribution')[[1]][2]), overwrite=T)
+        #### climate impacts
+        raster(crs = crs(rs.sp), vals = 0, resolution = c(1, 1), ext = extent(c(-180, 180, -90, 90))) %>%
+          rasterize(rs.sp, ., field='ClimImp', fun='first') %>% 
+          writeRaster(., paste0('ClimCon_species/Species_ClimImp/',s,
+                                strsplit(spe,'SpeciesDistribution')[[1]][2]), overwrite=T)
+        
+        print(paste0('----Complete: ',s,'-',strsplit(spe,'/')[[1]][4],
+                     '-',strsplit(spe,'/')[[1]][5],'----'))
       }
-      
-      ### export as raster files
-      rs.sp <- st_as_sf(spe_cc, coords = c('x','y'), crs=4326) %>% 
-        as(., "Spatial") 
-      #### climate connectivity
-      raster(crs = crs(rs.sp), vals = 0, resolution = c(1, 1), ext = extent(c(-180, 180, -90, 90))) %>%
-        rasterize(rs.sp, ., field='ClimCon', fun='first') %>% 
-        writeRaster(., paste0('ClimCon_species/Species_ClimCon/',s,
-                              strsplit(spe,'SpeciesDistribution')[[1]][2]), overwrite=T)
-      #### climate impacts
-      raster(crs = crs(rs.sp), vals = 0, resolution = c(1, 1), ext = extent(c(-180, 180, -90, 90))) %>%
-        rasterize(rs.sp, ., field='ClimImp', fun='first') %>% 
-        writeRaster(., paste0('ClimCon_species/Species_ClimImp/',s,
-                              strsplit(spe,'SpeciesDistribution')[[1]][2]), overwrite=T)
-      
-      print(paste0('----Complete: ',s,'-',strsplit(spe,'/')[[1]][4],
-                   '-',strsplit(spe,'/')[[1]][5],'----'))
     }
+    
   }
 }
 
