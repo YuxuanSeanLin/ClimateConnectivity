ab4c484d1413023df411f722900ab7eee4e44656
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-03-01 00:40:26 +0800

    calculate species-based connectivity

--- Linkage/Analysis/step4_ccSpecies.R
+++ Linkage/Analysis/step4_ccSpecies.R
@@ -51,20 +51,47 @@ for (h in c('surface','mesopelagic','bathypelagic','abyssopelagic')){
           
           ### calculate climate connectivity and climate impacts (against climate of designated patches)
           if (length(which(Dest_temp>=low_end & Dest_temp<=high_end))>0){ #### has suitable destinations
+            #### climate connectivity traverse
             Dest_temp <- Dest_temp[which(Dest_temp>=low_end & Dest_temp<=high_end)] #### highest extremes (capability)
-            spe_bp$ClimCon <- unique(links$From_pre) - min(Dest_temp)
+            Dest_temp <- min(Dest_temp)
+            spe_bp$ClimCon <- unique(links$From_pre) - Dest_temp
             spe_bp$ClimImp <- 0
           }else{ #### has no suitable destinations
             #### targeted patches with lowest extremes (minimized stress)
             dest_id <- which(abs(unique(links$From_pre)-Dest_temp)==min(abs(unique(links$From_pre)-Dest_temp)))
             #### calculation
-            spe_bp$ClimCon <- abs(unique(links$From_pre)-Dest_temp[dest_id])*(-1)
-            if (Dest_temp[dest_id]>high_end){
-              spe_bp$ClimImp <- (Dest_temp[dest_id]-high_end)/(high_end-low_end)
+            Dest_temp <- Dest_temp[dest_id]
+            spe_bp$ClimCon <- abs(unique(links$From_pre)-Dest_temp)*(-1)
+            if (Dest_temp>high_end){
+              spe_bp$ClimImp <- (Dest_temp-high_end)/(high_end-low_end)
             }else{
-              spe_bp$ClimImp <- (low_end-Dest_temp[dest_id])/(high_end-low_end)
+              spe_bp$ClimImp <- (low_end-Dest_temp)/(high_end-low_end)
             }
           }
+          
+          ### calculate connectivity coefficient with cumulative LCP pathways divide cost-weighed distance
+          dest_fut <- links[,(ncol(links)/2+2):ncol(links)] %>% as.data.frame()
+          conn_coef <- c()
+          for (r in 1:nrow(dest_fut)){
+            cum_CWD <- 0
+            cum_LCP <- 0
+            pathway_temp <- dest_fut[r,] %>% as.numeric()
+            if (length(which(pathway_temp==Dest_temp))!=0){
+              pathway_id <- links[r,1:(which(pathway_temp==Dest_temp)+1)] %>% as.numeric()
+              
+              #### calculate cumulative LCP and CWD
+              for (k in 1:(length(pathway_id)-1)){
+                from_p <- pathway_id[k]
+                to_p <- pathway_id[k+1]
+                cum_CWD <- cum_CWD + LCP_list$CW_Dist[which(LCP_list$Origin==from_p & LCP_list$Dest==to_p)]
+                cum_LCP <- cum_LCP + LCP_list$LCP_Length[which(LCP_list$Origin==from_p & LCP_list$Dest==to_p)]
+              }
+              #### calculate coefficiency
+              conn_coef <- append(conn_coef, cum_LCP/cum_CWD)
+            }
+          }
+          spe_bp$ConnCoef <- max(conn_coef)
+          
           ### combine table
           spe_cc <- rbind(spe_cc, spe_bp)
         }
@@ -81,6 +108,11 @@ for (h in c('surface','mesopelagic','bathypelagic','abyssopelagic')){
         raster(crs = crs(rs.sp), vals = 0, resolution = c(1, 1), ext = extent(c(-180, 180, -90, 90))) %>%
           rasterize(rs.sp, ., field='ClimImp', fun='first') %>% 
           writeRaster(., paste0('ClimCon_species/Species_ClimImp/',s,
+                                strsplit(spe,'SpeciesDistribution')[[1]][2]), overwrite=T)
+        #### connectivity coefficient
+        raster(crs = crs(rs.sp), vals = 0, resolution = c(1, 1), ext = extent(c(-180, 180, -90, 90))) %>%
+          rasterize(rs.sp, ., field='ConnCoef', fun='first') %>% 
+          writeRaster(., paste0('ClimCon_species/Species_ConnCoef/',s,
                                 strsplit(spe,'SpeciesDistribution')[[1]][2]), overwrite=T)
         
         print(paste0('----Complete: ',s,'-',strsplit(spe,'/')[[1]][4],
@@ -106,20 +138,24 @@ for (s in c('ssp126','ssp245','ssp370','ssp585')){
     ## import canvas (180*360 raster, value=0)
     canvas_cc <- raster('canvas.tif')
     canvas_ci <- canvas_cc
+    canvas_lcp <- canvas_cc
     
     ## list of all species (read recursively)
     spelist <- list.files(paste0('Species_ClimCon/',s,'/',h), 
                           full.names = T, recursive = T, pattern = '.tif$')
     
     for (spe in spelist){
-      ## load connectivity and CI
+      ## load connectivity, CI and LCP coefficient
       ClimCon <- raster(spe)
       ClimImp <- raster(paste0('Species_ClimImp',
                                strsplit(spe,'Species_ClimCon')[[1]][2]))
+      ConnCoef <- raster(paste0('Species_ConnCoef',
+                                strsplit(spe,'Species_ClimCon')[[1]][2]))
       
       ## sum all
       canvas_cc <- sum(canvas_cc, ClimCon, na.rm = T)
       canvas_ci <- sum(canvas_ci, ClimImp, na.rm = T)
+      canvas_lcp <- sum(canvas_lcp, ConnCoef, na.rm = T)
       
       print(paste0('----Complete: ',s,'-',strsplit(spe,'/')[[1]][4],
                    '-',strsplit(spe,'/')[[1]][5],'----'))
@@ -128,12 +164,15 @@ for (s in c('ssp126','ssp245','ssp370','ssp585')){
     ## extract canvas by mask (boundary)
     canvas_cc_r <- mask(canvas_cc, mask_r)
     canvas_ci_r <- mask(canvas_ci, mask_r)
+    canvas_lcp_r <- mask(canvas_lcp, mask_r)
     
     ## export data
     writeRaster(canvas_cc_r, paste0('Species_ClimCon_total/',h,
                                     '/Tot_ClimCon_',h,'_',s,'.tif'),overwrite = TRUE)
     writeRaster(canvas_ci_r, paste0('Species_ClimImp_total/',h,
                                     '/Tot_ClimImp_',h,'_',s,'.tif'),overwrite = TRUE)
+    writeRaster(canvas_lcp_r, paste0('Species_ConnCoef_total/',h,
+                                     '/Tot_ConnCoef_',h,'_',s,'.tif'),overwrite = TRUE)
     
     print(paste0('----Complete: ',h,'----'))
   }
@@ -178,7 +217,7 @@ for (h in c('surface','mesopelagic','bathypelagic','abyssopelagic')){
 
 
 #####
-# Step4: calculate mean connectivity (total connectivity / richness)
+# Step4: calculate mean connectivity and LCP coefficient (total / richness) (set mean connectivity as an example)
 
 for (h in c('surface','mesopelagic','bathypelagic','abyssopelagic')){
   # load species richness
