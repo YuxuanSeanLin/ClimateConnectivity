40685b936d2cb2d85b0b35d2826da3e45e81eb09
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-10-21 17:58:27 +0800

    Update and rename mv_Average.py to mva_idw.py

--- CMIP6/mva_idw.py
+++ CMIP6/mva_idw.py
@@ -6,6 +6,9 @@ import arcpy
 from arcpy import env
 
 wd = ''
+
+## moving window averaging ##
+
 for depth in ['surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic']:
     for scen in ['ssp126', 'ssp245', 'ssp370', 'ssp585']:
         from_path = '%s/thetao_expand/%s/%s' % (wd, depth, scen)
@@ -20,4 +23,19 @@ for depth in ['surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic']:
         print("======== %s - %s: complete ========" % (depth, scen))
 
         
+## IDW interpolation ##
+
+for depth in ['surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic']:
+    for scen in ['ssp126', 'ssp245', 'ssp585']:
+        env.workspace = '%s/thetao_exp2p/%s/%s' % (wd, depth, scen)
+        os.chdir('%s/thetao_exp2p/%s/%s' % (wd, depth, scen))
+        files = sorted(glob("*shp"))
+        for f in files:
+            name = f.split('.shp')[0]
+            to_path = "%s/thetao_idw/%s/%s/%s.tif" % (wd, depth, scen, name)
+
+            with arcpy.EnvManager(mask="%s/bathymetry/%s.tif" % (wd, depth), extent="-180 -90 180 90"):
+                arcpy.gp.Idw_sa(f, "value", to_path, "1", "3", "VARIABLE 12", "")
+
+        print("%s: %s --- complete" % (depth, scen)) 
         
