c72dfb790a91583bd1780e8909d558a8ab494d52
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-10-21 17:52:44 +0800

    Update and rename step4_movingWin.py to mv_Average.py

--- CMIP6/mv_Average.py
+++ CMIP6/mv_Average.py
@@ -8,10 +8,10 @@ from arcpy import env
 wd = ''
 for depth in ['surface', 'mesopelagic', 'bathypelagic', 'abyssopelagic']:
     for scen in ['ssp126', 'ssp245', 'ssp370', 'ssp585']:
-        from_path = '%s/5_data_expand/%s/%s' % (wd, depth, scen)
-        to_path = '%s/6_data_smooth/%s/%s' % (wd, depth, scen)
+        from_path = '%s/thetao_expand/%s/%s' % (wd, depth, scen)
+        to_path = '%s/thetao_smooth/%s/%s' % (wd, depth, scen)
         for f in os.listdir(from_path):
-            masktif = '%s/4_data_mdmean/%s/%s/%s' % (wd, depth, scen, f)
+            masktif = '%s/thetao_smooth/%s/%s/%s' % (wd, depth, scen, f)
             with arcpy.EnvManager(mask=masktif):
                 out_raster = arcpy.ia.FocalStatistics("%s/%s" % (from_path, f), 
                                                       "Rectangle 3 3 CELL", "MEAN"); 
