b14b992ebcc69949fa139855b4c607f181033f4a
YuxuanSeanLin <94066615+YuxuanSeanLin@users.noreply.github.com>
2022-11-27 00:54:50 +0800

    Update step1_cdo.sh

--- CMIP6/step1_cdo.sh
+++ CMIP6/step1_cdo.sh
@@ -1,12 +1,10 @@
 # Processed by Climate Data Operator (https://code.mpimet.mpg.de/projects/cdo)
-# GFDL-ESM4 example
 
-cd /mnt/hgfs/cmip6/data/thetao_add
+cd /mnt/hgfs/cmip6
 
-for yr in {2095..2100};do
-ifile="thetao_Omon_GFDL-ESM4_ssp126_r1i1p1f1_gn_209501-210012.nc"
-ofile="${yr}/thetao_Omon_GFDL-ESM4_ssp126_r1i1p1f1_gn_209501-210012_${yr}.nc"
-cdo -yearmean -sellonlatbox,-180,180,-90,90 -remapbil,r360*180 -selyear,${yr} ${ifile} ${ofile}
+for yr in {2020..2100};do
+ifile="<filename>"
+ofile="<filename>"
+cdo -yearmean -sellonlatbox,-180,180,-90,90 -remapbil,r360×180 -selyear,${yr} ${ifile} ${ofile}
 echo $ofile
 done
-
